e7561f1dba94e407615a06b0a8f66104982cf384
Wjyuy <zhzhwm@naver.com>
2025-04-07 11:37:40 +0900

    20250407_examspringbootfile

---
+++ item_mybatis_oracle/src/main/java/com/lgy/item_mybatis_oracle/dao/ItemDAO.java
@@ -0,0 +1,36 @@
+package com.lgy.item_mybatis_oracle.dao;
+
+import java.util.ArrayList;
+
+import com.lgy.item_mybatis_oracle.dto.ItemDTO;
+
+//public class ItemDAO {
+public interface ItemDAO {
+	
+	public void write(final String name, final String price, final String description);
+	public ArrayList<ItemDTO> list();
+	
+}
+	/*
+	public void write(final String name, final String price, final String description) {
+		template.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				String sql="insert into item(name, price, description"
+						+ ") values(?,?,?)";
+				PreparedStatement pstmt=con.prepareStatement(sql);
+				pstmt.setString(1, name);
+				pstmt.setString(2, price);
+				pstmt.setString(3, description);
+				return pstmt;
+			}
+		});
+	}
+	
+	public ArrayList<ItemDTO> list() {
+		String sql="select name, price, description from item";
+		return (ArrayList<ItemDTO>) template.query(sql, new BeanPropertyRowMapper(ItemDTO.class));
+	}
+}
+	 */
